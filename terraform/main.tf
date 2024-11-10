ef32f02b74ea9db14eb2fb0165b7e7e7b1d9fa5d
ShaikAliyaUmmar <alizehshaik91@gmail.com>
2024-11-10 19:05:25 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,29 @@
 provider "aws" {
-  region = "us-east-1" # Change this to your preferred region
+  region = "us-east-1"
 }
  
 # Elastic Beanstalk Application
-resource "aws_elastic_beanstalk_application" "my_python_app" {
-  name        = "my-python-app"
-  description = "A sample Elastic Beanstalk application running Python"
-}
- 
-# Elastic Beanstalk Application Version
-resource "aws_elastic_beanstalk_application_version" "app_version" {
-  application = aws_elastic_beanstalk_application.my_python_app.name
-  version_label = "v1"
-  # AWS S3 bucket containing the sample application provided by AWS
-  s3_bucket = "elasticbeanstalk-samples-us-west-2"  # Make sure this matches the provider region
-  s3_key    = "python-sample.zip"
+resource "aws_elastic_beanstalk_application" "example" {
+  name        = "simple-app"
+  description = "A simple Elastic Beanstalk application"
 }
  
 # Elastic Beanstalk Environment
-resource "aws_elastic_beanstalk_environment" "my_python_env" {
-  name                = "my-python-env"
-  application         = aws_elastic_beanstalk_application.my_python_app.name
-  version_label       = aws_elastic_beanstalk_application_version.app_version.version_label
-  solution_stack_name = "64bit Amazon Linux 2 v3.4.11 running Python 3.8"  # Use the latest Python platform version
+resource "aws_elastic_beanstalk_environment" "example_env" {
+  name                = "simple-env"
+  application         = aws_elastic_beanstalk_application.example.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.4.10 running Python 3.8"  # Use an available platform
  
-  # Environment configuration
-  settings {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "InstanceType"
-    value     = "t3.micro"  # Specify instance type as required
-  }
- 
-  settings {
-    namespace = "aws:elasticbeanstalk:environment"
+  # Enable the sample application provided by AWS
+  setting {
+    namespace = "aws:elasticbeanstalk:application"
     name      = "EnvironmentType"
     value     = "SingleInstance"
+  }
+ 
+  setting {
+    namespace = "aws:elasticbeanstalk:application"
+    name      = "ApplicationName"
+    value     = "Sample Application"  # Specifies AWS's default sample application
   }
 }

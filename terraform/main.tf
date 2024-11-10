a785cce2da7f48793128c1cdad1eab0f82435860
ShaikAliyaUmmar <alizehshaik91@gmail.com>
2024-11-10 20:50:59 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"  # Change to your desired region
+  region = "us-east-1"
 }
  
 # IAM Role for Elastic Beanstalk
@@ -25,6 +25,12 @@ resource "aws_iam_role_policy_attachment" "elastic_beanstalk_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
 }
  
+# Create Instance Profile for Elastic Beanstalk
+resource "aws_iam_instance_profile" "elastic_beanstalk_instance_profile" {
+  name = "aws-elasticbeanstalk-ec2-role-profile"
+  role = aws_iam_role.elastic_beanstalk_role.name
+}
+ 
 # Elastic Beanstalk Application
 resource "aws_elastic_beanstalk_application" "my_app" {
   name        = "my-sample-app"
@@ -35,18 +41,18 @@ resource "aws_elastic_beanstalk_application" "my_app" {
 resource "aws_elastic_beanstalk_environment" "my_env" {
   name                   = "my-sample-env"
   application            = aws_elastic_beanstalk_application.my_app.name
-  solution_stack_name    = "64bit Amazon Linux 2023 v4.3.0 running Python 3.12"  # Use an available stack
-  wait_for_ready_timeout  = "5m"  # Optional
+  solution_stack_name    = "64bit Amazon Linux 2023 v4.3.0 running Python 3.12"  # Compatible stack
+  wait_for_ready_timeout = "10m"  # Extended timeout for environment readiness
+ 
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.elastic_beanstalk_instance_profile.name
+  }
  
   setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "EnvironmentType"
     value     = "SingleInstance"
-  }
- 
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = aws_iam_role.elastic_beanstalk_role.name
   }
 }

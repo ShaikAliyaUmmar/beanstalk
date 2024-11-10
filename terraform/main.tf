5253ecb5e8864473b0e0e1a72c2593d4453c79b3
ShaikAliyaUmmar <alizehshaik91@gmail.com>
2024-11-10 20:02:12 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,8 +12,8 @@ resource "aws_elastic_beanstalk_application" "example" {
 resource "aws_elastic_beanstalk_environment" "example_env" {
   name                   = "simple-env"
   application            = aws_elastic_beanstalk_application.example.name
-  solution_stack_name    = "64bit Amazon Linux 2 running Python 3.8"  # Ensure this matches an available stack in us-east-1
-  wait_for_ready_timeout = "20m"
+  solution_stack_name    = "64bit Amazon Linux 2023 v4.3.0 running Python 3.12"  # Ensure this matches an available stack in us-east-1
+  #wait_for_ready_timeout = "20m"
  
   # Environment configuration settings
   setting {

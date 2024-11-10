e052e5e998403320bd0f72decd11b49c26393eaf
ShaikAliyaUmmar <alizehshaik91@gmail.com>
2024-11-10 20:56:59 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,20 +20,32 @@ resource "aws_iam_role" "elastic_beanstalk_role" {
   })
 }
  
+resource "aws_iam_instance_profile" "elastic_beanstalk_profile" {
+  name = "aws-elasticbeanstalk-ec2-role"
+  role = aws_iam_role.elastic_beanstalk_role.name
+}
+ 
+# Attach Elastic Beanstalk policy to the role
 resource "aws_iam_role_policy_attachment" "elastic_beanstalk_policy" {
   role       = aws_iam_role.elastic_beanstalk_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
 }
  
-# Create Instance Profile for Elastic Beanstalk
-resource "aws_iam_instance_profile" "elastic_beanstalk_instance_profile" {
-  name = "aws-elasticbeanstalk-ec2-role-profile"
-  role = aws_iam_role.elastic_beanstalk_role.name
+# Launch Template with IMDSv1 enabled
+resource "aws_launch_template" "beanstalk_template" {
+  name          = "beanstalk-template"
+  instance_type = "t3.micro"
+  iam_instance_profile {
+    name = aws_iam_instance_profile.elastic_beanstalk_profile.name
+  }
+  metadata_options {
+    http_tokens = "optional"  # This enables IMDSv1
+  }
 }
  
 # Elastic Beanstalk Application
 resource "aws_elastic_beanstalk_application" "my_app" {
-  name        = "my-sample-app"
+  name        = "my-sample-app-1"
   description = "A sample application for Elastic Beanstalk"
 }
  
@@ -41,18 +53,26 @@ resource "aws_elastic_beanstalk_application" "my_app" {
 resource "aws_elastic_beanstalk_environment" "my_env" {
   name                   = "my-sample-env"
   application            = aws_elastic_beanstalk_application.my_app.name
-  solution_stack_name    = "64bit Amazon Linux 2023 v4.3.0 running Python 3.12"  # Compatible stack
-  wait_for_ready_timeout = "10m"  # Extended timeout for environment readiness
- 
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = aws_iam_instance_profile.elastic_beanstalk_instance_profile.name
-  }
+  solution_stack_name    = "64bit Amazon Linux 2023 v4.3.0 running Python 3.11"  # Use an available stack
+  wait_for_ready_timeout = "10m"
  
   setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "EnvironmentType"
     value     = "SingleInstance"
   }
+ 
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.elastic_beanstalk_profile.name
+  }
+ 
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "LaunchTemplate"
+    value     = aws_launch_template.beanstalk_template.id
+  }
 }
+
+has context menu

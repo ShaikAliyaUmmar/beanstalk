4cdd9b9e9f8e8c8725561d93115c6816b8fd3292
ShaikAliyaUmmar <alizehshaik91@gmail.com>
2024-11-10 19:19:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ resource "aws_elastic_beanstalk_application" "example" {
 resource "aws_elastic_beanstalk_environment" "example_env" {
   name                = "simple-env"
   application         = aws_elastic_beanstalk_application.example.name
-  solution_stack_name = "Python 3.12 running on 64bit Amazon Linux 2023 4.3.0"  # Use an available platform
+  aws elasticbeanstalk list-platform-versions --filters "PlatformName=Python" "PlatformVersion=3.12"
  
   # Enable the sample application provided by AWS
   setting {

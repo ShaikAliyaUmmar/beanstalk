90d5d931b60be2794cbe7e6f61643046b58d54e6
ShaikAliyaUmmar <alizehshaik91@gmail.com>
2024-11-10 20:13:19 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,17 +8,47 @@ resource "aws_elastic_beanstalk_application" "example" {
   description = "A simple Elastic Beanstalk application"
 }
  
+# IAM Role for Elastic Beanstalk EC2 instances
+resource "aws_iam_role" "eb_instance_role" {
+  name = "aws-elasticbeanstalk-ec2-role"
+ 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+ 
+# Attach Policy to Role
+resource "aws_iam_role_policy_attachment" "eb_instance_policy" {
+  role       = aws_iam_role.eb_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+}
+ 
+# Create Instance Profile
+resource "aws_iam_instance_profile" "eb_instance_profile" {
+  name = "aws-elasticbeanstalk-ec2-profile"
+  role = aws_iam_role.eb_instance_role.name
+}
+ 
 # Elastic Beanstalk Environment
 resource "aws_elastic_beanstalk_environment" "example_env" {
-  name                   = "simple-env"
-  application            = aws_elastic_beanstalk_application.example.name
-  solution_stack_name    = "64bit Amazon Linux 2023 v4.3.0 running Python 3.12"  # Ensure this matches an available stack in us-east-1
-  #wait_for_ready_timeout = "20m"
+  name                = "simple-env"
+  application         = aws_elastic_beanstalk_application.example.name
+  solution_stack_name = "64bit Amazon Linux 2 running Python 3.8"
+  wait_for_ready_timeout = "20m"
  
-  # Environment configuration settings
+  # Associate instance profile
   setting {
-    namespace = "aws:elasticbeanstalk:environment"
-    name      = "EnvironmentType"
-    value     = "SingleInstance"
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.eb_instance_profile.name
   }
 }

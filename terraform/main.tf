4016b0c4a014fa5844a530e10cdf550504c23725
ShaikAliyaUmmar <alizehshaik91@gmail.com>
2024-11-10 19:43:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ resource "aws_elastic_beanstalk_environment" "example_env" {
   name                = "simple-env"
   application         = aws_elastic_beanstalk_application.example.name
   #platform_arn = "arn:aws:elasticbeanstalk:region::platform/Python 3.12 running on 64bit Amazon Linux 2/version"
-  solution_stack_name = "64bit Amazon Linux 2 v3.5.6 running Python 3.12"
+  solution_stack_name = "64bit Amazon Linux 2 v3.4.10 running Python 3.8"
 
   # Enable the sample application provided by AWS
   setting {
